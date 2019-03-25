ddaaae693aebad35ba2ee11738b21ad51ef67f2f
aurelijaa <aurelija@czv.lt>
2019-03-25 18:47:15 +0200

    webpack configuration

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import timestamp from './timestamp';
+import randomNumber from './randomNumber';
+import './index.css';
+import './index.scss';
+console.log(timestamp());
+console.log(randomNumber());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'biuld'),
+    filename: 'index.bundle.js',
+  },
+  module: {
+    rules: [
+      {test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader']},
+      {test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']},
+    ],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new MiniCssExtractPlugin({filename: 'index.bundle.css'}),
+  ],
+  mode: 'production',
+};
